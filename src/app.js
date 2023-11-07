2a477d30ed3f29fd880329d86cbc6bc1ac1ea169
ensarsnr <dev.anise@outlook.com>
2023-11-07 22:26:41 +0300

    Connected MongoDB

--- src/app.js
+++ src/app.js
@@ -1,12 +1,28 @@
+const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("../src/db/database");
+const mongoose = require("mongoose");
 const userRoutes = require("./routes/user");
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(cors());
+
 app.use(bodyParser.json());
+
+mongoose
+  .connect("mongodb://localhost:27017/chatdb", {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("MongoDB'ye başarıyla bağlandı");
+  })
+  .catch((err) => {
+    console.error("MongoDB bağlantı hatası:", err);
+  });
+
 app.use("/user", userRoutes);
 
 app.listen(port, () => {
