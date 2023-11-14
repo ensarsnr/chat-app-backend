642e99ad79a21abf7341659e3e5229f0c619a6f0
ensarsnr <dev.anise@outlook.com>
2023-11-14 23:25:24 +0300

    Created user routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,48 @@ const express = require("express");
 const router = express.Router();
 const User = require("../model/user");
 
+// getting user's name
+router.post("/get-name", async (req, res) => {
+  const { userId } = req.body;
+
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found." });
+    }
+
+    const name = user.name;
+    const surname = user.surname;
+    const pic = user.pic;
+    res.json({ name, surname, pic });
+  } catch (error) {
+    console.error("Error getting user's name:", error.message);
+    res.status(500).json({ error: "Failed to get user's name." });
+  }
+});
+
+router.get("/all-friends/:userId", async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    // Kullanıcıyı bul
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: "Kullanıcı bulunamadı." });
+    }
+
+    const friendIds = user.friends;
+    const friends = await User.find({ _id: { $in: friendIds } });
+
+    res.json(friends);
+  } catch (error) {
+    console.error("Arkadaşları getirirken hata oluştu:", error.message);
+    res.status(500).json({ error: "Arkadaşları getirme başarısız oldu." });
+  }
+});
+
 // Added friends endpoint
 router.post("/add-friends/:userId", async (req, res) => {
   const { userId } = req.params;
