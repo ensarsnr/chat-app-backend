cacf2067aceeaa16058795ac31e06c7c2493ec46
ensarsnr <dev.anise@outlook.com>
2023-11-18 17:01:59 +0300

    Example message schema

--- src/model/message.js
+++ src/model/message.js
@@ -16,3 +16,32 @@ const MessageSchema = new mongoose.Schema(
 );
 
 module.exports = mongoose.model("Message", MessageSchema);
+
+// const mongoose = require("mongoose");
+
+// const MessageSchema = new mongoose.Schema(
+//   {
+//     sender: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "User",
+//       required: true,
+//     },
+//     content: {
+//       type: String,
+//       required: true,
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// MessageSchema.virtual("senderName", {
+//   ref: "User",
+//   localField: "sender",
+//   foreignField: "_id",
+//   justOne: true,
+// });
+
+// MessageSchema.set("toObject", { virtuals: true });
+// MessageSchema.set("toJSON", { virtuals: true });
+
+// module.exports = mongoose.model("Message", MessageSchema);
